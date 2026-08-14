a24053ca19bec488f277f91af76194ec648bfe0f
itsritesh02 <itsritesh02@gmail.com>
2026-08-14 11:52:29 +0530

    paymenrt route upd

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,9 +1,25 @@
+
 import express from "express";
-import { createRazorpayOrder } from "../controllers/paymentController.js";
+
+import {
+  createRazorpayOrder,
+  verifyRazorpayPayment,
+} from "../controllers/paymentController.js";
+
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create-order", protect, createRazorpayOrder);
+router.post(
+  "/create-order",
+  protect,
+  createRazorpayOrder
+);
+
+router.post(
+  "/verify",
+  protect,
+  verifyRazorpayPayment
+);
 
 export default router;
